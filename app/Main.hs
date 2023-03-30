b193f67137a45f9a793c699370a84603ab35f1d5
Nicolas Mori <nicolas.mori@outlook.de>
2023-03-30 15:36:46 +0200

    Get HCL running

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main (main) where
 
+import System.Console.HCL
 import Lib
 
 main :: IO ()
-main = someFunc
+main = execReq $ reqConfirm confirm $ reqForever topMenu
+    where
+        confirm = promptAgree "Are you sure you want to quit? " (Just False) reqResp
+
+someRes :: Request ()
+someRes = reqIO someFunc
+
+topMenu :: Request ()
+topMenu = reqMenu $
+    reqMenuItem "fun" (reqIO someFunc) $
+    reqMenuItem "test" notImpl $
+    reqMenuEnd
+
+notImpl :: Request ()
+notImpl = reqIO $ putStrLn "This function is not implemented."
